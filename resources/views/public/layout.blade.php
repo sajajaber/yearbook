4f916cb2e4dd8eb95642bd4ea9a8a824a772d1f0
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-10 11:17:01 +0300

    Load global responsive styles

--- resources/views/public/layout.blade.php
+++ resources/views/public/layout.blade.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Merriweather:wght@400;700&display=swap" rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/css/responsive.css', 'resources/js/app.js'])
     @yield('extra-css')
 
     <style>
