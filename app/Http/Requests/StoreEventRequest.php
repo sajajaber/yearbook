05bdcd4164e380bf65f0858c5234d4037e83f8f1
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-13 03:17:19 +0300

    Sanitize event rich text before validation

--- app/Http/Requests/StoreEventRequest.php
+++ app/Http/Requests/StoreEventRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use App\Support\RichText;
 
 class StoreEventRequest extends FormRequest
 {
@@ -12,21 +13,17 @@ class StoreEventRequest extends FormRequest
         if ($this->user()?->role?->role_name === 'editor') {
             $this->merge(['status' => 'draft']);
         }
+
+        $this->merge([
+            'description' => RichText::sanitize($this->input('description')),
+        ]);
     }
 
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
