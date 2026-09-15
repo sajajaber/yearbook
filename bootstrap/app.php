ca231a620a93cc85eeab42e9d169b3399384311d
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-15 14:03:42 +0300

    Polish media upload feedback styling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->append(\App\Http\Middleware\InjectGraduateProfileExtensions::class);
+        $middleware->append(\App\Http\Middleware\StyleMediaFeedback::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
