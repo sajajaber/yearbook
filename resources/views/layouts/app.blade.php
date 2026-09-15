aefb4eab33e31c90c43669f0b8ad7101da755d9b
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-15 14:22:12 +0300

    Load standard alert styling in admin layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Merriweather:wght@400;700&display=swap" rel="stylesheet">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/alerts.css') }}">
 
     <style>
         /* ========================================
