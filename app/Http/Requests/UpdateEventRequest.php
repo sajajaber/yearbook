89d61846a5d8e3fa7f298627354d5e5f8aabddf6
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-16 15:47:59 +0300

    Fix editor event updates to preserve workflow status

--- app/Http/Requests/UpdateEventRequest.php
+++ app/Http/Requests/UpdateEventRequest.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
+use App\Models\Event;
 use Illuminate\Foundation\Http\FormRequest;
 use App\Support\RichText;
 
@@ -12,6 +12,11 @@ class UpdateEventRequest extends FormRequest
     {
         if ($this->user()?->role?->role_name === 'editor') {
             $event = $this->route('event');
+
+            if (! $event instanceof Event) {
+                $event = Event::find($event);
+            }
+
             $this->merge(['status' => $event?->status]);
         }
 
