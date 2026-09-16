27835ad4d57381c3e80c05594790940c2ae1edbc
Saja Jaber <136196859+sajajaber@users.noreply.github.com>
2026-09-16 15:34:39 +0300

    Remove self-service account deletion route

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,6 @@ Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['au
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 Route::middleware(['auth', 'verified', 'role:admin'])->group(function () {
     Route::get('settings', [SettingsController::class, 'index'])->name('settings.index');
